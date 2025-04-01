88d8d2b69e4af6acc5f8134d36b8ba523a9a5b84
Gregory Zavalnitskiy <gzavalnitskiy@gmail.com>
2025-03-31 23:08:08 -0400

    adding new script and config

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   try {
     const body = JSON.parse(event.body as string);
     const email = body.email;
-    const response = await fetch('https://script.google.com/macros/s/AKfycbwczMlyR-zd9ciujQkB-id_d2RPJWIWsOg02vtqzKeEaASZrdyziZSc6zSQOjp8soPMgw/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbxYdabddBg9oWcuU4EO8oVYVlcI_wkn6soWi0LxuubGQxEpIWciXS4abnnhxkaNY9XHDw/exec', {
       method: 'POST',
       body: JSON.stringify({ email }),
       headers: { 'Content-Type': 'application/json' }
@@ -20,7 +20,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     console.error('Error during form submission:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'An error occurred. Please try again.' }),
+      body: JSON.stringify({ message: `An error occurred. Please try again. Error: ${JSON.stringify(error)} with body ${JSON.stringify(event)}` })
     };
   }
 };
